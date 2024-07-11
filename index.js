d0efd715f328b1fcc906c2cbb0759919520e54f3
jagedos <jagedosusrecomm@gmail.com>
2024-07-11 22:16:13 +0300

    js arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+let num1 = 2;
+
+let num2 =31;
+
+let multiply = num1*num2;
+
+console.log(multiply);
+
+let random = Math.floor(Math.random()*Number.MAX_SAFE_INTEGER)+1;
+
+console.log(random);
+
+let num3 = 24;
+
+let num4 = 10;
+
+let mod = num3%num4;
+
+console.log(mod);
+
+let arr = [2,20,16,9];
+
+let max = Math.max(...arr);
+
+console.log(max);
